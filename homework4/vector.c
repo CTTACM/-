90a394b8d4c222a00046592c7126a597646a6d48
CTTACM <1247846793@qq.com>
2017-12-24 21:39:55 +0800

    Update vector.c

--- homework4/vector.c
+++ homework4/vector.c
@@ -1,57 +1,41 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <assert.h>
-#include <sys/time.h>
-#include <math.h>
-#include <accel.h>
-#include <accelmath.h>
+void vecaddgpu( float *r, float *a, float *b, int n )
+{
+    #pragma acc kernels loop copyin(a[0:n],b[0:n]) copyout(r[0:n])
+    for( int i = 0; i < n; ++i ) r[i] = a[i] + b[i];
+}
 
 int main( int argc, char* argv[] )
 {
-    int n;      /* size of the vector */
-    float *restrict a;  /* the vector */
-    float *restrict r;  /* the results */
-    float *restrict e;  /* expected results */
-    float s, c;
-    struct timeval t1, t2, t3;
-    long cgpu, chost;
-    int i;
-    if( argc > 1 )
-        n = atoi( argv[1] );
-    else
-        n = 100000;
+    int n; /* vector length */
+    float * a; /* input vector 1 */
+    float * b; /* input vector 2 */
+    float * r; /* output vector */
+    float * e; /* expected output values */
+    int i, errs;
+    if( argc > 1 ) n = atoi( argv[1] );
+    else n = 100000; /* default vector length */
     if( n <= 0 ) n = 100000;
-
-    a = (float*)malloc(n*sizeof(float));
-    r = (float*)malloc(n*sizeof(float));
-    e = (float*)malloc(n*sizeof(float));
-    for( i = 0; i < n; ++i ) a[i] = (float)(i+1) * 2.0f;
-    /*acc_init( acc_device_nvidia );*/
-
-    gettimeofday( &t1, NULL );
-    #pragma acc parallel loop copyin(a[0:n]) copyout(r[0:n])
+    a = (float*)malloc( n*sizeof(float) );
+    b = (float*)malloc( n*sizeof(float) );
+    r = (float*)malloc( n*sizeof(float) );
+    e = (float*)malloc( n*sizeof(float) );
     for( i = 0; i < n; ++i ){
-        s = sinf(a[i]);
-        c = cosf(a[i]);
-        r[i] = s*s + c*c;
-    }
-    gettimeofday( &t2, NULL );
-    cgpu = (t2.tv_sec - t1.tv_sec)*1000000 + (t2.tv_usec - t1.tv_usec);
-    
+    a[i] = (float)(i+1);
+    b[i] = (float)(1000*i);
+}
+/* compute on the GPU */
+vecaddgpu( r, a, b, n );
+/* compute on the host to compare */
+for( i = 0; i < n; ++i ) e[i] = a[i] + b[i];
+    /* compare results */
+    errs = 0;
     for( i = 0; i < n; ++i ){
-        s = sinf(a[i]);
-        c = cosf(a[i]);
-        e[i] = s*s + c*c;
+    if( r[i] != e[i] ){
+    ++errs;
     }
-    gettimeofday( &t3, NULL );
-    chost = (t3.tv_sec - t2.tv_sec)*1000000 + (t3.tv_usec - t2.tv_usec);
-    /* check the results */
-    for( i = 0; i < n; ++i )
-        assert( fabsf(r[i] - e[i]) < 0.000001f );
-    
-    
-    printf( "%13d iterations completed\n", n );
-    printf( "%13ld microseconds on GPU\n", cgpu );
-    printf( "%13ld microseconds on host\n", chost );
-    return 0;
+}
+printf( "%d errors found \n", errs );
+return errs;
 }
